47611caad4804acda29a8742bc4907dbc9e2b14c
jos25506 <joselinemora7070@gmail.com>
2026-09-17 20:45:25 -0600

    personalizacion layout

--- JN_API/JN_API/obj/Debug/net10.0/JN_API.AssemblyInfo.cs
+++ JN_API/JN_API/obj/Debug/net10.0/JN_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JN_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e819f69033b82eaaa08ddfe43a805884d4e11242")]
 [assembly: System.Reflection.AssemblyProductAttribute("JN_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JN_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
